7bf68bf30d403995934eef495fc157154b7d6d0a
arielmirra <amirra@mulesoft.com>
2021-09-08 20:24:09 -0300

    feat: set EpicID from profile page

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,41 +1,143 @@
-import useUser from "../../hooks/useUser"
 import { useFormik } from "formik"
-import * as Yup from "yup"
-import React from "react"
-import Avatar from "../../components/Avatar"
-import { getEpicIds, setEpicId } from "../../firebase/client"
+import React, { useState } from "react"
+import { setEpicID } from "../../firebase/client"
 import { MatButton, MatTextField } from "../../hooks/formik"
-import { Button, TextField } from "@material-ui/core"
+import {
+  Grid,
+  Avatar,
+  Typography,
+  makeStyles,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  IconButton,
+  CircularProgress,
+} from "@material-ui/core"
+import { Create as CreateIcon } from "@material-ui/icons"
+import withUser from "wrappers/withUser"
 
-export default function ProfilePage() {
-  const user = useUser()
+const useStyles = makeStyles((theme) => ({
+  large: {
+    height: "100px",
+    width: "100px",
+  },
+}))
+
+const stats = async (epicID) => {
+  const api =
+    "https://api.tracker.gg/api/v2/rocket-league/standard/profile/epic/"
+  const res = await fetch(api + epicID, {
+    headers: {
+      "user-agent":
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.58 Safari/537.36",
+    },
+  })
+  const data = await res.json()
+  if (res.status === 404) {
+    return { notFound: true }
+  } else return data
+}
+
+function ProfilePage({ user, setUser }) {
+  const classes = useStyles()
+  const [isModalOpen, setModalOpened] = useState(false)
+  const [submitting, setSubmitting] = useState(false)
+
+  const closeModal = () => setModalOpened(false)
+  const openModal = () => setModalOpened(true)
 
   const formik = useFormik({
     initialValues: {
-      epicId: "",
+      epicID: user?.epicID ?? "",
     },
-    // validationSchema: Yup.object({
-    //   epicId: Yup.string().required("Required"),
-    // }),
-    onSubmit: (values) => {
-      setEpicId(user.uid, values.epicId)
-      getEpicIds(user.uid).then((db) => console.log(db))
-      // console.log(getEpicIds(user.uid))
+    onSubmit: async (values) => {
+      setSubmitting(true)
+      await setEpicID(user.uid, values.epicID)
+      setUser({ ...user, epicID: values.epicID })
+      closeModal()
+      setSubmitting(false)
     },
   })
 
   return (
     <>
-      <Avatar width={100} />
-      <form onSubmit={formik.handleSubmit}>
-        <MatTextField inputId="epicId" formik={formik} label="Epic ID" />
-        <MatButton text="GUARDAR EPIC ID" type="submit" />
-      </form>
+      <Grid container direction="row" spacing={2}>
+        <Grid container item xs={4} justifyContent="center">
+          <Avatar
+            className={classes.large}
+            alt={user.username}
+            src={user.avatar}
+            title={user.username}
+          />
+        </Grid>
+        <Grid item xs={8} container direction="column" justifyContent="center">
+          <Typography variant="h5">{user.username}</Typography>
+          {user.epicID && (
+            <Grid container direction="row" alignItems="center" spacing={2}>
+              <Grid item>
+                <Typography variant="h6">{user.epicID}</Typography>
+              </Grid>
+              <Grid item>
+                <IconButton onClick={openModal}>
+                  <CreateIcon />
+                </IconButton>
+              </Grid>
+            </Grid>
+          )}
+        </Grid>
+      </Grid>
+      {!user.epicID && (
+        <div id="save-epic-id" className="center-content">
+          <MatButton onClick={openModal} text="Ingresá tu Epic ID" />
+        </div>
+      )}
+      <Dialog
+        open={isModalOpen}
+        onClose={closeModal}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">Ingresá tu Epic ID</DialogTitle>
+        <DialogContent>
+          {submitting ? (
+            <div className="center-content">
+              <CircularProgress />
+            </div>
+          ) : (
+            <form onSubmit={formik.handleSubmit}>
+              <MatTextField inputId="epicID" formik={formik} label="Epic ID" />
+            </form>
+          )}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={closeModal} color="primary" disabled={submitting}>
+            Cancelar
+          </Button>
+          <Button
+            onClick={formik.submitForm}
+            color="primary"
+            autoFocus
+            disabled={submitting}
+          >
+            Guardar
+          </Button>
+        </DialogActions>
+      </Dialog>
       <style jsx>{`
-        form {
-          padding: 15px;
+        #save-epic-id {
+          flex: 1;
+          flex-direction: column;
+        }
+
+        .center-content {
+          display: flex;
+          place-content: center;
         }
       `}</style>
     </>
   )
 }
+
+export default withUser(ProfilePage)
