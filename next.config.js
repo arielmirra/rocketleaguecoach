204d75547b2ed583d18e204c54ca656d003d2175
arielmirra <amirra@mulesoft.com>
2021-09-16 23:01:49 -0300

    feat: add internationalization

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextTranslate = require("next-translate")
+
+const nextConfig = {
+  reactStrictMode: true,
+  i18n: {
+    locales: ["es"],
+    defaultLocale: "es",
+    pages: {
+      "*": ["common"],
+    },
+  },
+}
+
+module.exports = nextTranslate(nextConfig)
